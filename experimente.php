541c7ac45e5a23a874630ca593967516f104411c
Well Luiz <wellington.luiz@tecnofit.com.br>
2021-10-23 19:57:43 -0300

    atualizacoes pro jao

--- experimente.php
+++ experimente.php
@@ -1,7 +1,7 @@
 <?php include 'header.php';?>
 <?php include 'price.php';?>
-<div class="container-fluid px-0 mx-0" style="margin-top:65px">
-    <?php include 'menu.php';?>
+<div class="container-fluid px-0 mx-0" style="margin-top:100px">
+    <?php include 'configuracao.php';?>
 </div>
 
     <div class="container-fluid py-32 px-0">
@@ -9,62 +9,137 @@
             <img src="img/img/step1.png"/>
         </div>
         <div class="container d-flex justify-content-center align-items-center text-center">
-            <a href="step2_1.php"><h1 class="title"><strong>Escolha</strong><br> o estilo</h1></a>
+            <a href="step2_1.php"><h1 class="title"><strong>Escolha</strong><br> seu sofa</h1></a>
         </div>
         <div class="container d-flex justify-content-center align-items-center text-center">
             <a class="arrow-down" href="step2_1.php"><img src="img/svg/down.svg"/></a>
         </div>
     </div>
-
-    <div id="moma" class="container-fluid bg-black py-32 px-0">
-        <div class="container d-flex justify-content-center pb-32 text-center flex-column">
-            <h1>Moma</h1>
-            <p>Para quem ama design e não abre mão do conforto.</p>
-            <div class="container d-flex justify-content-center">
-                <a href="step2_1.php" class="px-2">Saiba mais ></a>
-                <a href="step2_1.php" class="px-2">Personalizar ></a>
-            </div>
-        </div>
-        <?php include 'configuracao.php';?>
-    </div>
-
-    <div id="clausi" class="container-fluid py-32 px-0">
-        <div class="container d-flex justify-content-center pb-32 text-center flex-column">
-            <h1>Clausi</h1>
-            <p>Para os minimalistas de plantão.</p>
-            <div class="container d-flex justify-content-center">
-                <a href="step2_1.php" class="px-2">Saiba mais ></a>
-                <a href="step2_1.php" class="px-2">Personalizar ></a>
-            </div>
-        </div>
-        <?php include 'configuracao.php';?>
-    </div>
-
-    <div id="tuim" class="container-fluid bg-tuim py-32 px-0">
-        <div class="container d-flex justify-content-center pb-32 text-center flex-column">
-            <h1>Tuim</h1>
-            <p>O clássico amigão do Netflix</p>
-            <div class="container d-flex justify-content-center">
-                <a href="step2_1.php" class="px-2">Saiba mais ></a>
-                <a href="step2_1.php" class="px-2">Personalizar ></a>
-            </div>
-        </div>
-        <?php include 'configuracao.php';?>
-    </div>
-
     
+    <div class="container d-flex justify-content-center text-center flex-column">
+        <button class="btn-colored d-flex justify-content-between">
+            <span class="cart-icon"></span>
+            Experimentar
+        </button>
+        <p class="compre">Compre e experimente em casa por 10 dias</p>
 
-    <div id="comparando" class="container-fluid py-32">
-        <div class="container d-flex justify-content-center text-center">
-            <h1 class="title"><strong>Comparando</strong> os estilos</h1>
+        <p>de R$ 4.529,75</p>
+        <p>por <strong>R$ 4.029,75</strong> à vista <strong>(5% de desconto)</strong> ou</p>
+        <h1 class="text-dark fw-bold">10 X R$ 424,18</h1>
+    </div>
+
+    <div class="accordion container-fluid p-0" id="meusofa">
+        <div class="accordion-item">
+            <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button fw-bold" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Meu sofá
+            </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div class="accordion-body p-0 m-0">
+                    <div class="meusofa-item px-4 pt-4 pb-2 d-flex flex-column">
+                        <p class="title">Estilo</p>
+                        <p class="subtitle">Moma</p>
+                    </div>
+                    <div class="meusofa-item px-4 pt-4 pb-2 d-flex flex-column">
+                        <p class="title">Formato</p>
+                        <p class="subtitle">3 assentos retráteis</p>
+                    </div>
+                    <div class="meusofa-item px-4 pt-4 pb-2 d-flex flex-column">
+                        <p class="title">Medida</p>
+                        <p class="subtitle">3,00m</p>
+                    </div>
+                    <div class="meusofa-item px-4 pt-4 pb-2 d-flex flex-column">
+                        <p class="title">Braço</p>
+                        <p class="subtitle">25cm</p>
+                    </div>
+                    <div class="meusofa-item px-4 pt-4 pb-2 d-flex flex-column">
+                        <p class="title">Tecido</p>
+                        <p class="subtitle">Linho</p>
+                    </div>
+                    <div class="meusofa-item px-4 pt-4 pb-2 d-flex flex-column">
+                        <p class="title">Cor</p>
+                        <p class="subtitle">Azul Linho</p>
+                    </div>
+                    <div class="meusofa-item px-4 pt-4 pb-2 d-flex flex-column">
+                        <p class="title">Enchimento assentos</p>
+                        <p class="subtitle">Estável e Macio</p>
+                    </div>
+                    <div class="meusofa-item px-4 pt-4 pb-2 d-flex flex-column">
+                        <p class="title">Abertura assentos</p>
+                        <p class="subtitle">Automático</p>
+                    </div>
+                    <div class="meusofa-item px-4 pt-4 pb-2 d-flex flex-column">
+                        <p class="title">Carregador USB</p>
+                        <p class="subtitle">USB braço direito</p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="container blue-border p-4">
-           <h1 class="title text-center"><strong>Medida mínima</strong></h1>
-           <?php include 'medidas.php';?>
+    </div>
+
+    <div class="container d-flex justify-content-center text-center flex-column py-32">
+        <a href="#">Alterar seu sofá ></a>
+    </div>
+
+    <div id="propaganda-card" class="container-fluid py-32">
+        <div class="container d-flex flex-wrap">
+            <div class="col-6 p-0 m-0 d-flex text-center flex-column align-items-center ">
+                <img src="img/svg/truck.svg"/>
+                <p class="fw-bold">Frete Grátis</p>
+                <p>para diversos estados do Brasil</p>
+                <a href="#">Saiba mais ></a>
+            </div>
+
+            <div class="col-6 p-0 m-0 d-flex text-center flex-column align-items-center">
+                <img src="img/svg/casa.svg"/>
+                <p class="fw-bold">10 dias para experimentar</p>
+                <a href="#">Saiba mais ></a>
+            </div>
+
+            <div class="col-6 p-0 m-0 d-flex text-center flex-column align-items-center">
+                <img src="img/svg/selo.svg"/>
+                <p class="fw-bold">2 anos de garantia</p>
+                <a href="#">Saiba mais ></a>
+            </div>
+
+            <div class="col-6 p-0 m-0 d-flex text-center flex-column align-items-center">
+                <img src="img/svg/chat.svg"/>
+                <p class="fw-bold">Ajuda para comprar</p>
+                <p>Converse com uma especialista online ou ligue para: <a href="tel:43991025500">43 9 9102 5500</a></p>
+                <a href="#">Saiba mais ></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container d-flex justify-content-center text-center flex-column">
+        <button class="btn-colored d-flex justify-content-between">
+            <span class="cart-icon"></span>
+            Experimentar
+        </button>
+        <p class="compre">Compre e experimente em casa por 10 dias</p>
+    </div>
+
+    <div class="container-fluid py-32 bt-gray footer-experiment">
+        <div class="container d-flex flex-column align-items-center text-center">
+            <img src="img/svg/chat.svg"/>
+            <p>Se preferir, tire suas dúvidas em uma experiência online, e receba ótimas dicas de decoração das Consultoras de Design ;)</p>
+            <a href="#">Agendar demosntração online ></a>
         </div>
     </div>
     
-    <?php include 'consultor.php';?>
-    <?php include 'consultor_pessoal.php';?>
+    <div class="container-fluid py-32 bt-gray footer-experiment">
+        <div class="container d-flex flex-column align-items-center text-center">
+            <img src="img/svg/paper.svg"/>
+            <p>Está em dúvida no tecido? Enviamos amostras para sua casa. Selecione até 3 opções e receba-as em até 5 dias úteis.</p>
+            <a href="#">Solicitar amostras ></a>
+        </div>
+    </div>
 
-<?php include 'footer.php';?>
+    <div class="container-fluid py-32 bt-gray footer-experiment">
+        <div class="container d-flex flex-column align-items-center text-center">
+            <img src="img/svg/market.svg"/>
+            <p>Ou experimente na <strong>Soft Guide Shop Londrina</strong> e conheça tudo o que precisa para montar o sofá perfeito para você.</p>
+            <a href="#">Agendar visita ></a>
+        </div>
+    </div>
