ac4a184d252d008200947e259ba4bb812e91ff52
Well Luiz <wellington.luiz@tecnofit.com.br>
2021-11-29 10:06:49 -0300

    atualizaçao da tabela de medidas

--- medidas.php
+++ medidas.php
@@ -3,47 +3,220 @@
         <h1 class="title text-center"><strong>Medida mínima</strong></h1>
 
         <div class="card" style="width: 18rem;">
-            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
-            <p class="text-center border-bottom pb-3 f-14">LXAXCXm</p>
             <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
-            <p class="text-center border-bottom pb-3 f-14">LXAXCXm</p>
+            <p class="text-center border-bottom pb-3 f-14">2,00m</p>
             <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
-            <p class="text-center pb-3 f-14">LXAXCXm</p>
+            <p class="text-center border-bottom pb-3 f-14">1,80m</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">1,80m</p>
         </div>
     </div>
+
     <div class="item d-flex justify-content-center align-items-center flex-column">
-    <h1 class="title text-center"><strong>Medida mínima</strong></h1>
+        <h1 class="title text-center"><strong>Medida máxima</strong></h1>
 
         <div class="card" style="width: 18rem;">
-            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
-            <p class="text-center border-bottom pb-3 f-14">LXAXCXm</p>
             <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
-            <p class="text-center border-bottom pb-3 f-14">LXAXCXm</p>
+            <p class="text-center border-bottom pb-3 f-14">4,10m</p>
             <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
-            <p class="text-center pb-3 f-14">LXAXCXm</p>        
+            <p class="text-center border-bottom pb-3 f-14">4,30m</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">4,10m</p>
         </div>
     </div>
+
     <div class="item d-flex justify-content-center align-items-center flex-column">
-    <h1 class="title text-center"><strong>Medida mínima</strong></h1>
+        <h1 class="title text-center"><strong>Profundidade c/ retrátil aberto</strong></h1>
 
         <div class="card" style="width: 18rem;">
-            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
-            <p class="text-center border-bottom pb-3 f-14">LXAXCXm</p>
             <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
-            <p class="text-center border-bottom pb-3 f-14">LXAXCXm</p>
+            <p class="text-center border-bottom pb-3 f-14">1,80m</p>
             <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
-            <p class="text-center pb-3 f-14">LXAXCXm</p>           </div>
+            <p class="text-center border-bottom pb-3 f-14">1,54m</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">1,91m</p>
+        </div>
     </div>
+
     <div class="item d-flex justify-content-center align-items-center flex-column">
-    <h1 class="title text-center"><strong>Medida mínima</strong></h1>
+        <h1 class="title text-center"><strong>Profundidade mínima fechado</strong></h1>
 
         <div class="card" style="width: 18rem;">
-            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
-            <p class="text-center border-bottom pb-3 f-14">LXAXCXm</p>
             <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
-            <p class="text-center border-bottom pb-3 f-14">LXAXCXm</p>
+            <p class="text-center border-bottom pb-3 f-14">1,20m</p>
             <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
-            <p class="text-center pb-3 f-14">LXAXCXm</p>           
+            <p class="text-center border-bottom pb-3 f-14">1,00m</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">1,31m</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Altura total</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">95cm c/ encosto posição 3<br>107cm c/ encosto posição 7</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">88cm</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">104cm c/ encosto posição 3<br>115cm c/ encosto posição 7</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Tipo almofada do encosto</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">Solta</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">Solta</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">Fixa</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Tipo de encosto</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">Reclinável</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">Não reclinável</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">Reclinável</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Opção de assento fixo</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">Não</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">Sim</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">Não</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Altura do chão ao assento</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">46cm</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">43cm</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">46cm</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Altura almofada do encosto</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">61cm</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">45cm</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">75cm</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Opções de assentos</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">Estável e Macio<br>Flutuante</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">Estável e Macio<br>Flutuante<br>Super Macio</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">Estável e Macio<br>Flutuante<br>Super Macio</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Opcional abertura automática</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">Sim</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">Não</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">Sim</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Opcional  massageador</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">Não</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">Não</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">Sim</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Opção USB</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">Sim</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">Sim</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">Sim</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Opção impermeável</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">Sim</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">Sim</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">Sim</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Opção canto</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">Sim</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">Sim</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">Não</p>
+        </div>
+    </div>
+
+    <div class="item d-flex justify-content-center align-items-center flex-column">
+        <h1 class="title text-center"><strong>Largura mínima dos braços</strong></h1>
+
+        <div class="card" style="width: 18rem;">
+            <p class="estilo m-0 pt-4 pb-1 text-center">Moma</p>
+            <p class="text-center border-bottom pb-3 f-14">25cm</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Clausi</p>
+            <p class="text-center border-bottom pb-3 f-14">10cm</p>
+            <p class="estilo m-0 pt-4 pb-1 text-center">Tuim</p>
+            <p class="text-center pb-3 f-14">10cm</p>
         </div>
     </div>
 </div>
